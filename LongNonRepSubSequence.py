35da4e22b60dcf5a26dfdd1850ecc4184636a1dc
Utpal Mattoo <30846264+UtpalMattoo@users.noreply.github.com>
2023-05-19 08:17:30 -0400

    Update LongNonRepSubSequence.py
    print the long string and it's length (not only length) - this is to compare with some other versions

--- LongNonRepSubSequence.py
+++ LongNonRepSubSequence.py
@@ -28,7 +28,7 @@ class Solution(object):
         for char in s:
             found_char, lst = self.found (char, lst)                                                          
             new_lst.append(lst)
-        print (new_lst)    
+        #print (new_lst)    
         d = defaultdict(lambda: 0)
         for elem in new_lst:
             for item in elem:
@@ -36,7 +36,19 @@ class Solution(object):
                 d[item[-1]] = len(item[-1]) -1    
               else:
                 d[item]  = len(item)
-        print (d.values())
+        #print (d.values())
+        
+        #qucik fix to test
+        lst1 = [ (key, val) for key, val in d.items()]
+        #print (lst1)
+        max_init = 0        
+        long_str = ''        
+        for item in lst1:
+            if item[1] > max_init:
+                max_init = item[1]
+                long_str = item[0]            
+        print (f" long_str = {long_str}, max_len = {max_init}")
+        
         val = max(d.values())
         return val
 
@@ -61,10 +73,9 @@ class Solution(object):
                       new_lst.append(char)                  
             return found_char, new_lst 
 
-# s = "abbbb"
-# s = "abbac"
-# s = "peewcwwkec"
-# s = "aaxbcdcdax"  
-s = "hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789hijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+#s = "abbbb"
+#s = "abbac"
+s = "peewcwwkec"
+#s = "aaxbcdcdaxjlijliewleuwieuuvs;pwq"  
 subseq_len = Solution().lengthOfLongestSubstring(s)
 print (subseq_len)
